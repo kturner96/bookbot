76967b0214228be2dc37453968dc6c1050af6049
Keiran Turner <keiranturner54@gmail.com>
2025-06-15 23:23:16 +0100

    Added features

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-from stats import num_words
+from stats import (get_book_text, num_words, character_repeat, sort_dict)
+import sys
 
-num_words()
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {filename}...")
+print("----------- Word Count ----------")
+num_words(filename)
+
+print("----------- Character Count ----------")
+
+text = get_book_text(filename)
+counts = character_repeat(text)
+sorted_report = sort_dict(counts)
+
+for entry in sorted_report :
+    print(f"{entry['char']}: {entry['num']}")
+
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -1,12 +1,33 @@
-def get_book_text(path_to_file) :
-    with open(path_to_file) as f:
+def get_book_text(filename) :
+    with open(filename) as f:
         file_contents = f.read()
         return file_contents
     
-def num_words() :
-    split_words = get_book_text("books/frankenstein.txt").split()
+def num_words(filename) :
+    split_words = get_book_text(filename).split()
     num_words = len(split_words)
-    print(f"{num_words} words found in the document")
+    print(f"Found {num_words} total words")
 
-def character_repeat() :
-    None
+def character_repeat(text) :
+    lowercase = text.lower()
+    
+    characters_dict = {}
+
+    for char in lowercase :
+        if char in characters_dict :
+            characters_dict[char] += 1
+        else :
+            characters_dict[char] = 1
+
+    return characters_dict
+    print(characters_dict)
+
+def sort_dict(characters_dict) :
+    result_list = []
+
+    for char, count in characters_dict.items() :
+        if char.isalpha():
+            result_list.append({"char": char, "num": count})
+
+    result_list.sort(key=lambda x: x["num"], reverse=True)
+    return result_list
